1da494a3fd8490d0b3c988b957023707207a7e0a
Mark Flarup <mflarup@gmail.com>
2016-08-26 08:43:28 +0000

    Tried to change stuff in a file

--- http.go
+++ http.go
@@ -13,8 +13,8 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func to_roman(n int)  string {
-    if n == 2 {
-        return "II"
+    if n == 3 {
+        return "III"
     }
     return "I"
 }
